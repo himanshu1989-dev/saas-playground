67aca82324e594df1b6181d19aace528252b6a5c
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-13 22:46:02 -0500

    some html customization 43

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const PORT = 3333;
 const server = http.createServer((req,res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.end('Hello World');
 })
 
