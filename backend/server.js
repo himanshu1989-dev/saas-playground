623a1d29e0b58c5b8b9b4e3eaf22a5b6c4d088e6
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-13 23:04:16 -0500

    some html customization 44

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,10 @@ const server = http.createServer((req,res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type','text/plain');
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end('Hello World');
-})
+    res.end(JSON.stringify({
+        status: "ok",
+        message: "Recipe Assistant backend is running"}));
+});
 
 
 server.listen(PORT,"0.0.0.0",() => {
