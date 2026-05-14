8a4598132994aa2951ad8f3ef3f931e6673dfc0a
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-13 23:05:18 -0500

    some html customization 45

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
-const http = require('node:http');
+const http = require("node:http");
 
 const PORT = 3333;
-const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/plain');
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(JSON.stringify({
-        status: "ok",
-        message: "Recipe Assistant backend is running"}));
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.end(
+    JSON.stringify({
+      status: "ok",
+      message: "Recipe Assistant backend is running",
+    }),
+  );
 });
 
-
-server.listen(PORT,"0.0.0.0",() => {
-    console.log(`Server running at http://0.0.0.0:${PORT}/`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running at http://0.0.0.0:${PORT}/`);
 });
