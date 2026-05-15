017f2398a87484c3d070c6115f8574acce03edcd
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-14 21:18:43 -0500

    some html customization 46

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,28 @@ const http = require("node:http");
 
 const PORT = 3333;
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
+  console.log(`Received request: ${req.method} ${req.url}`);
+
+  if (req.url == "/api/health")
+  {
+  res.statusCode = 200;
   res.end(
     JSON.stringify({
       status: "ok",
       message: "Recipe Assistant backend is running",
     }),
   );
+}else{
+  res.statusCode = 404;
+  res.end(
+    JSON.stringify({
+      error: "Endpoint not found",
+    }),
+  );
+}
 });
 
 server.listen(PORT, "0.0.0.0", () => {
