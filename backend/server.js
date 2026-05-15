a17e6ff75431688c5362f6672574b7d14fa42e62
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-14 22:19:22 -0500

    some html customization 49

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,37 @@
 const http = require("node:http");
+const recipes = require("./data/recipes.json");
 
 const PORT = 3333;
 const server = http.createServer((req, res) => {
-
   res.setHeader("Content-Type", "application/json");
   res.setHeader("Access-Control-Allow-Origin", "*");
   console.log(`Received request: ${req.method} ${req.url}`);
 
-  if (req.url == "/api/health")
-  {
-  res.statusCode = 200;
-  res.end(
-    JSON.stringify({
-      status: "ok",
-      message: "Recipe Assistant backend is running",
-    }),
-  );
-}else{
-  res.statusCode = 404;
-  res.end(
-    JSON.stringify({
-      error: "Route not found",
-    }),
-  );
-}
+  if (req.method === "GET" && req.url == "/api/health") {
+    res.statusCode = 200;
+    res.end(
+      JSON.stringify({
+        status: "ok",
+        message: "Recipe Assistant backend is running",
+      }),
+    );
+  } else if (req.method === "GET" && req.url === "/api/recipes") {
+    for (let i = 0; i < recipes.length; i++) {
+      if (req.url.includes(recipes[i].dishId)) {
+        res.statusCode = 200;
+        res.end(JSON.stringify(recipes[i].ingredients));
+        res.end(JSON.stringify(recipes[i].steps));
+        return;
+      }
+    }
+  } else {
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        error: "Route not found",
+      }),
+    );
+  }
 });
 
 server.listen(PORT, "0.0.0.0", () => {
