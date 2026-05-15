1fe5464562500ce4190ab4251950f470f4f0c220
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-15 07:59:02 -0500

    some html customization 55

--- backend/services/recipeService.js
+++ backend/services/recipeService.js
@@ -1,11 +1,12 @@
+import recipes from "../data/recipes.json" with { type: "json" };
+
 export function findRecipeByDishId(dishId) {
-    const recipes = require("../data/recipes.json");
-    for (let i = 0; i < recipes.length; i++) {
-        if (recipes[i].dishId === dishId) {
-            return {
-                ingredients: recipes[i].ingredients,
-                steps: recipes[i].steps,
-            };
-        }
+  for (let i = 0; i < recipes.length; i++) {
+    if (recipes[i].dishId === dishId) {
+      return {
+        ingredients: recipes[i].ingredients,
+        steps: recipes[i].steps,
+      };
     }
+  }
 }
