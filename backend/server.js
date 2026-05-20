e7b6d40c8dff50b0ae0d08ccb5aec2b5073eb295
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-19 22:58:52 -0500

    some html customization 60

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,20 @@ const PORT = 3333;
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "application/json");
+
+  // CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
   console.log(`Received request: ${req.method} ${req.url}`);
+
+  // Browser may send OPTIONS request before POST
+  if (req.method === "OPTIONS") {
+    res.statusCode = 204;
+    res.end();
+    return;
+  }
 
   if (!dishRoute(req, res)) {
     res.statusCode = 404;
