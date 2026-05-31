257197eaf30173129f49fa328aced12b9a288089
himanshu1989 <himanshu1989.dev@gmail.com>
2026-05-31 13:58:54 -0500

    some html customization 74

---
+++ frontend/js/recipe.js
@@ -0,0 +1,35 @@
+/**
+ * recipe.js - this is responsible for building the recipe html page
+ * using the recipe realted information retrieved from the backend
+ */
+class RecipePageBuilder {
+  constructor(recipeString) {
+    this.recipe = JSON.parse(recipeString);
+  }
+  
+  createAndPopulateAboutDishSection() {
+    const aboutDishSection = document.createElement("section");
+    aboutDishSection.id = "idAboutDishSection";
+    aboutDishSection.classList.add("about-dish-section");
+    aboutDishSection.textContent = this.recipe.aboutDish;
+    document.body.appendChild(aboutDishSection);
+  }
+
+  createAndPopulateIngredientsSection(recipeOption) {
+    // Create the section element
+    const ingredientsSection = document.createElement("section");
+
+    ingredientsSection.id = "idIngredientsSection";
+
+    // Assign the CSS class already defined in CSS file
+    ingredientsSection.classList.add("ingredients-section");
+
+    // Add content later
+    ingredientsSection.textContent = recipeData
+  }
+  createAndPopulateNutritionSection(recipeOption) {}
+  createAndPopulateVideoSection(recipeOption) {}
+  createAndPopulateStepsSection(recipeOption) {}
+  createAndPopulateSocialMediaFeedbackSection(recipeOption) {}
+  createAndPopulateSimilarDishesSection(recipeOption) {}
+}
